fdb1b6f5f2b01f1d47c61c871cf74bbf190379ed
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-04-24 17:42:01 +0100

    regexpjs.js

---
+++ regexpjs.js
@@ -0,0 +1,13 @@
+var PEG, coffee, grammar, parser, r;
+
+PEG = require('pegjs');
+
+grammar = 'start = a: [aeiou\u2661]i . [^x-z] ';
+
+parser = PEG.generate(grammar, {});
+r = parser.parse('Abr');
+console.log(r);
+
+r = parser.parse('♡br');
+console.log(r);
+
