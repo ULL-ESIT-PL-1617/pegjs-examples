dc27ab661a9f414c1ea523791f59a2462495d361
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-04-24 17:39:26 +0100

    dotjs.js

--- ignorecasejs.js
+++ ignorecasejs.js
@@ -1,13 +1,12 @@
 "use strict"
-let r;
 
 const PEG = require('pegjs');
 
-const grammar = `start = a: 'a'i `;
+const grammar = `start = a:'a'i `;
 
-const parser = PEG.generate(grammar);
+let parser = PEG.generate(grammar);
 
-r = parser.parse('A');
+let r = parser.parse('A');
 console.log(r);
 
 parser = PEG.generate(grammar);
--- initializer.js
+++ initializer.js
@@ -1,12 +1,12 @@
 "use strict";
 const PEG = require("pegjs");
 const grammar = `
- {                             
+ {
    const util = require("util");     
-                               
+
    var g = "visible variable"; 
    console.log("Inside Initializer! options = "+util.inspect(options)); 
- }                             
+ }
  start = 'a' { console.log('g = '+g); return 1; } 
        /  &  { console.log('inside predicate: g = '+g); return true; } 'b' { return 2; }
 `;
