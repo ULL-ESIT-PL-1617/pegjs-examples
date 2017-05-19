3fb825cf8a22c307233369808daa18946dbf71d4
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-05-19 10:23:45 +0100

    acentos

---
+++ use_acentos.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+var PEG = require("./acentos.js");
+var r = PEG.parse('45');
+console.log(r);
+
--- use_arith2js.js
+++ use_arith2js.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 var PEG = require("./arith2js.js");
 var input = process.argv[2] || `
 a <- 4-2-1:
@@ -22,3 +23,13 @@ a+b+9+c:
 console.log(`Processing <${input}>`);
 r = PEG.parse(input);
 console.log(r);
+/*
+  Variable a not initialized at line 2
+  Variable c not initialized at line 4
+  Variable c not initialized at line 4
+  let $a, $b;
+
+  $a = 4-a-1;
+  $b = (a+1)/a;
+  a+b+9+c
+*/
